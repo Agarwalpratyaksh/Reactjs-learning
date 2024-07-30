68b6c5eeb1e3dbe8a4e274c3bf4a5747450ba5d2
Pratyaksh Agarwal <pratyaksh.agrwl@gmail.com>
2024-07-31 00:41:11 +0530

    theme switcher

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -7,7 +7,6 @@ import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <UserContextProvider>
       <h1>React context</h1>
